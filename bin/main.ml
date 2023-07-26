577181a79872b36ad6530b074bb921f1f23abcca
lmelkamu <lydiamelkamu24@gmail.com>
2023-07-26 15:41:58 +0000

    commands

--- bin/main.ml
+++ bin/main.ml
@@ -9,5 +9,13 @@ open! Async
    let contents = File_fetcher.fetch_exn how_to_fetch ~resource in List.iter
    (get_credits contents) ~f:print_endline] *)
 
+let command =
+  Command.group
+    ~summary:"A tool for playing the wikipedia game, and other utilities"
+    [ "game", Jeopardy_island.Game.game_command
+    ; "question", Jeopardy_island.Question.question_command
+    ]
+;;
+
 let _ = Cohttp_async.Client.get
 let () = Command_unix.run Jeopardy_island.Game.game_command
