37823d29997358413c8afd2e859ca87bd61c3ead
Roman Chenoweth <romanc22@vt.edu>
2023-07-26 15:48:04 +0000

    working on question

--- bin/main.ml
+++ bin/main.ml
@@ -1,20 +1,9 @@
 open! Core
 open! Async
 
-<<<<<<< HEAD
-let command =
-  Command.async
-    ~summary:
-      "given a website that generates questions, GET a question and the 4 \
-       associated answer choices"
-    (let%map_open.Command () = return () in
-     fun () -> return ())
-;;
-=======
 (* let command = Command.async ~summary: "given an IMDB page for an actor,
    print out a list of their main \ credits" (let%map_open.Command () =
    return () in fun () -> return ()) *)
->>>>>>> 36d27da9aa4e5de7bfa760dd9eaa9cba31c722dc
 
 (* [%map_open let how_to_fetch, resource = File_fetcher.param in fun () ->
    let contents = File_fetcher.fetch_exn how_to_fetch ~resource in List.iter
